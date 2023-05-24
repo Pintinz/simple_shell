f5991fc7270580f0686e8752ca5139347c541b95
fifithatlady <felistasfenyere2224@gmail.com>
2023-05-24 12:42:25 -0700

    gcc undefined

--- child.c
+++ child.c
@@ -16,21 +16,17 @@ int child(char *fullpath, char **tokens)
 	child_pid = fork();
 	if (child_pid == -1)
 	{
-	errors(1);
-	exit(EXIT_FAILURE);
+		errors(1);
+		exit(EXIT_FAILURE);
 	}
-
 	if (child_pid == 0)
 	{
-	execve_status = execve(fullpath, tokens, envp);
-	if (execve_status == -1)
-	return (-1);
+		execve_status = execve(fullpath, tokens, envp);
+		if (execve_status == -1)
+			return (-1);
 	}
 	else
-	{
-	wait(&status);
-	}
+		wait(&status);
 
 	return (0);
 }
-
